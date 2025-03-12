a31b18104befdcbddb09d7abe8abbdd812a51233
green-cr0w <africa1407@gmail.com>
2025-03-12 14:17:41 +0100

    some methods + dragon species

--- code/Dragon.ts
+++ code/Dragon.ts
@@ -10,5 +10,72 @@ export class Dragon {
     private readonly maxShots: number; //max shots
     private state: string; //current state of dragon, can change with hp
 
-    
+    constructor(name: string, description: string, dragonClass: string, firePower: number, physicalPower: number, health: number, maxHealth: number, shots: number, maxShots: number, state: string) {
+        this.name = name;
+        this.description = description;
+        this.class = dragonClass;
+        this.firePower = firePower;
+        this.physicalPower = physicalPower;
+        this.health = health;
+        this.maxHealth = maxHealth;
+        this.shots = shots;
+        this.maxShots = maxShots;
+        this.state = state;
+    }
+
+    //update the state of the dragon
+    updateState(state: string): void {
+        let checkup: number = this.health/this.maxHealth;
+        if (checkup == 1) {
+            this.state = this.getName() + 'looks healthy and ready to fight!';
+        } else if (checkup >= 0.5) {
+            this.state = this.getName() + 'looks a bit tired, but still ready to fight!';
+        }
+        else if (checkup >= 0.25) {
+            this.state = this.getName() + 'looks exhausted, maybe try to find some food and let it rest.';
+        }
+    }
+
+    //kill it with fire!
+    fireattack(): void {
+        if (this.shots > 0) {
+            this.shots--;
+            console.log(this.getName() + ' attacks with fire!');
+        } else {
+            console.log(this.getName() + ' is out of shots!');
+        }
+    }
+
+    //was that the bite of 87
+    physicalattack(): void {
+        console.log(this.getName() + ' attacks with a physical attack!');
+    }
+
+    //ouch
+    takeDamage(damage: number): void {
+        this.health -= damage;
+        console.log(this.getName() + ' took ' + damage + ' damage!');
+        if (this.health <= 0) {
+            this.health = 0;
+            console.log(this.getName() + ' has been defeated!');
+        }
+    }
+
+    //recover fireshots
+    recoverShots(): void {
+        this.shots = this.maxShots;
+        console.log(this.getName() + ' has recovered its shots!');
+    }
+
+    //i need a healer
+    recoverHealth(): void {
+        this.health = this.maxHealth;
+        console.log(this.getName() + ' has recovered its health!');
+    }
+
+
+    //getters and setters
+    getName(): string {
+        return this.name;
+    }
 }
--- code/DragonFactory.ts
+++ code/DragonFactory.ts
@@ -2,5 +2,32 @@ import { Dragon } from './Dragon';
 import { DragonSpecies } from './DragonSpecies';
 
 export class DragonFactory {
-    public 
+    public static createDragon(species: DragonSpecies, name: string): Dragon {
+        let dragon: Dragon;
+        switch (species) {
+            case DragonSpecies.CQ:
+                dragon = new Dragon(name, 'A fire breathing dragon', 'Fire', 10, 5, 100, 100, 10, 10, 'Healthy');
+                break;
+            case DragonSpecies.CH:
+                dragon = new Dragon(name, 'A frosty dragon', 'Ice', 5, 10, 100, 100, 10, 10, 'Healthy');
+                break;
+            case DragonSpecies.GG:
+                dragon = new Dragon(name, 'A poisonous dragon', 'Poison', 7, 7, 100, 100, 10, 10, 'Healthy');
+                break;
+            case DragonSpecies.TY:
+                dragon = new Dragon(name, 'A water breathing dragon', 'Water', 8, 8, 100, 100, 10, 10, 'Healthy');
+                break;
+            case DragonSpecies.WH:
+                dragon = new Dragon(name, 'A frosty dragon', 'Ice', 5, 10, 100, 100, 10, 10, 'Healthy');
+                break;
+            case DragonSpecies.SH:
+                dragon = new Dragon(name, 'An electric dragon', 'Electric', 9, 6, 100, 100, 10, 10, 'Healthy');
+                break;
+            case DragonSpecies.TC:
+                dragon = new Dragon(name, 'A wind dragon', 'Wind', 6, 9, 100, 100, 10, 10, 'Healthy');
+                break;
+        }
+        return dragon;
+    }
+        
 }
