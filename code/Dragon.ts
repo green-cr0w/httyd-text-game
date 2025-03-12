4243a398aa1fdbfbf420e452c8ac089bf057035f
green-cr0w <africa1407@gmail.com>
2025-03-12 11:44:36 +0100

    Initial commit

---
+++ code/Dragon.ts
@@ -0,0 +1,13 @@
+class Dragon {
+    private readonly name: string;
+    private readonly description: string;
+    private readonly firePower: number;
+    private readonly physicalPower: number;
+    private health: number;
+    private readonly maxHealth: number;
+    private shots: number;
+    private readonly maxShots: number;
+    private state: string;
+
+    
+}
