f85b8b8928789ee8b9a7a3abe4982745d076e87c
green-cr0w <africa1407@gmail.com>
2025-03-21 11:08:00 +0100

    fixed some methods from dragon + fixed complexity in factory

--- code/Dragon.ts
+++ code/Dragon.ts
@@ -32,70 +32,54 @@ export class Dragon {
 
     //update the state of the dragon
     public updateState(): void {
-        let checkup: number = this.health/this.maxHealth;
+        let checkup: number = this.getHealth()/this.getMaxHealth();
         if (checkup == 1) {
-            this.state = this.getName() + 'looks healthy and ready to fight!';
+            this.setState(this.getName() + `nuzzles your hand. Your dragon looks healthy and ready to explore!`);
         } else if (checkup >= 0.5) {
-            this.state = this.getName() + 'looks a bit tired, but still ready to fight!';
+            this.setState(this.getName() + `looks up at you with a bit of a tired expression, but still willing to keep going.`);
         }
         else if (checkup >= 0.25) {
-            this.state = this.getName() + 'looks exhausted, maybe try to find some food and let it rest.';
+            this.setState(this.getName() + `looks exhausted, maybe you should try to find some food and rest.`);
         }
     }
 
     //KILL IT WITH FIRE
     public fireattack(): void {
-        if (this.shots > 0) {
-            this.shots--;
-            console.log(this.getName() + ' attacks with fire!');
+        let actualShots: number = this.getShots();
+        if (actualShots > 0) {
+            this.setShots(actualShots - 1);
+            console.log(this.getName() + ` attacks with fire!`);
         } else {
-            console.log(this.getName() + ' is out of shots!');
+            console.log(this.getName() + ` is out of shots!`);
         }
     }
 
     //was that the bite of 87
     public physicalattack(): void {
-        console.log(this.getName() + ' attacks with a physical attack!');
-    }
-
-    //ouch
-    public takeDamage(damage: number): void {
-        this.health -= damage;
-        console.log(this.getName() + ' took ' + damage + ' damage!');
-        if (this.health <= 0) {
-            this.health = 0;
-            console.log(this.getName() + ' has been defeated!');
-        }
+        console.log(this.getName() + ` attacks with a physical attack!`);
     }
 
     //recover fireshots
     public recoverShots(): void {
-        this.shots = this.maxShots;
-        console.log(this.getName() + ' has recovered its shots!');
+        this.setShots(this.getMaxShots());
+        console.log(this.getName() + ` has recovered its shots!`);
     }
 
-    //i need a healer
-    public recoverHealth(): void {
-        this.health = this.maxHealth;
-        console.log(this.getName() + ' has recovered its health!');
-    }
-
-    //feeding dragon
+    //yummy time
     public feedDragon(food: Food): void {
-        if (food.getName() == 'Eel') {
-            if (this.eelEater == false) {
-                console.log(this.getName + ' does not eat eels!');
+        if (food.getName() == `Eel`) {
+            if (this.getEelEater() == false) {
+                console.log(this.getName + `'s eyes widen and give you an incredulous look before taking a few steps back. Right, dragons and their fear of eels...`);
             }
             else {
-                this.health += food.getHealing();
-                console.log(this.getName() + ' has eaten eel and recovered ' + food.getHealing() + ' health!');
+                console.log(this.getName() + ` looks at the eel in delight and immediatly gulps it down. It seems like your dragon really enjoyed that!`);
             }
         }
         else {
-            this.health += food.getHealing();
-            console.log(this.getName() + ' has eaten fish and recovered ' + food.getHealing() + ' health!');
+            console.log(this.getName() + ` looks at the ` + food.getName() + ` and eats it happily.`);            
         }
-        }
+        this.setHealth(food.getHealing());
+    }
 
     // #region Getters and Setters
     public getName(): string {
@@ -150,8 +134,26 @@ export class Dragon {
         return this.eelEater;
     }
 
-    public setHealth(health: number): void {
-        this.health = health;
+    public setHealth(healthAdded: number): void {
+        var actualHp: number = this.getHealth();
+        var maxHp: number = this.getMaxHealth();
+        var healed: number = healthAdded;
+        if (healthAdded < 0) {
+            this.health = Math.max(actualHp + healthAdded, 0);
+            if (this.getHealth() == 0) {
+                console.log(this.getName() + ` has been struck down!`);
+            }
+            else {
+                console.log(this.getName() + ` has taken ` + healed * -1 + ` points of damage!`); //* -1 to make it positive
+            }
+        }
+        else {
+            this.health = Math.min(actualHp + healthAdded, maxHp);
+                if (actualHp + healthAdded > maxHp) {
+                    healed = maxHp - actualHp;
+            }
+            console.log(this.getName() + ` has recovered ` + healed + ` points of its health!`);
+        }
     }
 
     public setShots(shots: number): void {
